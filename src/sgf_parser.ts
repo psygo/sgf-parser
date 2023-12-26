c1d36fbd08e047fdad47e2e47e62fe22e01bd0b5
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-26 09:13:11 -0300

    reorganizing and adding another example

--- src/sgf_parser.ts
+++ src/sgf_parser.ts
@@ -4,7 +4,7 @@ import { SgfTree } from "./sgf_tree";
  * An SGF tree is basically a *trie* data structure encoded
  * in text.
  */
-function parseSgf(sgf: string) {
+export function parseSgf(sgf: string) {
   // 1. Cleanup
   sgf = sgf
     .replaceAll("\n", "")
@@ -19,26 +19,31 @@ function parseSgf(sgf: string) {
 
   // 3. Flattened Recursion
   for (const char of sgf) {
-    if (char === "(") {
-      // 3.1. Opening a Branch
-      currentTree.data = currentString;
-      const newTree = new SgfTree(currentTree);
-      currentTree.children.push(newTree);
-      currentTree = newTree;
-      currentString = "";
-    } else if (char === ")") {
-      // 3.2. Closing the Current Branch and Going Back to the
-      //      Parent.
-      currentTree.data = currentString;
-      currentTree = currentTree.parent!;
-      currentString = currentTree.data;
-    } else {
-      currentString += char;
+    switch (char) {
+      case "(":
+        // 3.1. Opening a Branch
+        currentTree.data = currentString;
+        const newTree = new SgfTree(currentTree);
+        currentTree.children.push(newTree);
+        currentTree = newTree;
+        currentString = "";
+        break;
+      case ")":
+        // 3.2. Closing the Current Branch and Going Back to the
+        //      Parent.
+        currentTree.data = currentString;
+        currentTree = currentTree.parent!;
+        currentString = currentTree.data;
+        break;
+      default:
+        currentString += char;
     }
   }
 
   return trees.children;
 }
+
+function parseMovesAndMetadata(sgfData: string) {}
 
 const test1 = `
   (
@@ -81,8 +86,25 @@ const test3 = `
       )
   )
 `;
+const test4 = `
+  (
+    ;GM[1]FF[4]CA[UTF-8]AP[Sabaki:0.52.2]KM[6.5]SZ[19]DT[2023-12-25]
+    ;B[pd]C[Comment on move.]
+    ;W[dd]
+      (
+        ;B[pq]
+        ;W[dp]
+      )
+      (
+        ;B[dp]
+        ;W[pp]
+        ;PL[B]AE[jk]AB[jj]AW[ji]C[Comment on editing.]
+        ;B[jq]
+      )
+  )
+`;
 
-const sgf = parseSgf(test3);
+const sgf = parseSgf(test4);
 const sgfAsJSON = sgf.map((c) => c.toJSON());
 const prettyPrintSgf = JSON.stringify(sgfAsJSON, null, 2);
 
